8fde7026afb4d7a8e547acadc344c4813037d9d8
Jack Treado <john.treado@yale.edu>
2021-07-21 11:53:28 -0400

    sped up interface compression, Ftol now 1e-6

--- main/tumor2D/interfaceCompression.cpp
+++ main/tumor2D/interfaceCompression.cpp
@@ -9,11 +9,11 @@
 using namespace std;
 
 // global constants
-const double dphi0= 0.015;	   		// packing fraction increment during initial growth step
+const double dphi0= 0.01;	   		// packing fraction increment during initial growth step
 const double boxLengthScale = 2.0; 	// neighbor list box size in units of initial l0
 const double phi0 = 0.6;		   	// initial packing fraction
-const double dt0 = 2e-2;		   	// initial magnitude of time step in units of MD time
-const double Ftol = 1e-10;			// force tolerance during energy min
+const double dt0 = 5e-2;		   	// initial magnitude of time step in units of MD time
+const double Ftol = 1e-6;			// force tolerance during energy min
 const double Ptol = 1e-6;		   	// target pressure in initial compression
 
 
